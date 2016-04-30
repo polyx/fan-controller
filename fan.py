9c71c9e656cfe90e553f32abd73d5d9de8eb223c
Dmitry Filosofov <filosofov.dmitry@gmail.com>
2016-04-30 17:57:50 +0200

    Initial commit

---
+++ fan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+
+if len(sys.argv) > 1:
+	input = sys.argv[1]
+	print("seting fan speed...")
+	mycmd = subprocess.getoutput("echo level {}| sudo tee /proc/acpi/ibm/fan".format(input))
+	print("Return from acpi-driver:")
+	print(mycmd)
+else:
+	print("fan status is:")
+	status = subprocess.getoutput("cat /proc/acpi/ibm/fan")
+	print(status, "\n")
